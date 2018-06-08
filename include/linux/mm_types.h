9106a2b6e8e2981bed5e2cbc995fd4750ab119ef
Matthew Wilcox <mawilcox@microsoft.com>
2018-06-07 17:08:26 -0700

    UPSTREAM: mm: switch s_mem and slab_cache in struct page
    This will allow us to store slub's counters in the same bits as slab's
    s_mem.  slub now needs to set page->mapping to NULL as it frees the page,
    just like slab does.
    
    Link: http://lkml.kernel.org/r/20180518194519.3820-5-willy@infradead.org
    Change-Id: I841baf3a3b9a0e90c9baf7b0f99d4efa9c9f801a
    Signed-off-by: Matthew Wilcox <mawilcox@microsoft.com>
    Acked-by: Christoph Lameter <cl@linux.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Jérôme Glisse <jglisse@redhat.com>
    Cc: "Kirill A . Shutemov" <kirill.shutemov@linux.intel.com>
    Cc: Lai Jiangshan <jiangshanlai@gmail.com>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -86,7 +86,7 @@ struct page {
 		/* See page-flags.h for the definition of PAGE_MAPPING_FLAGS */
 		struct address_space *mapping;
 
-		void *s_mem;			/* slab first object */
+		struct kmem_cache *slab_cache;	/* SL[AU]B: Pointer to slab */
 		atomic_t compound_mapcount;	/* first tail page */
 		/* page_deferred_list().next	 -- second tail page */
 	};
@@ -197,7 +197,7 @@ struct page {
 		spinlock_t ptl;
 #endif
 #endif
-		struct kmem_cache *slab_cache;	/* SL[AU]B: Pointer to slab */
+		void *s_mem;			/* slab first object */
 	};
 
 #ifdef CONFIG_MEMCG
