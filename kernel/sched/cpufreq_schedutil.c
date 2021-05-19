2222f68c860308c994b305264f78f2e1a6492877
John Galt <johngaltfirstrun@gmail.com>
2021-05-19 09:02:30 -0400

    cpufreq: schedutil: Extend limit tunables to prime
    Also set sane defaults

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -728,6 +728,13 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->down_rate_limit_us =
 				CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
 
+	if (cpumask_test_cpu(policy->cpu, cpu_prime_mask)) {
+		tunables->up_rate_limit_us =
+					CONFIG_SCHEDUTIL_UP_RATE_LIMIT_PRIME;
+		tunables->down_rate_limit_us =
+					CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_PRIME;
+	}
+
 	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
 		tunables->up_rate_limit_us =
 					CONFIG_SCHEDUTIL_UP_RATE_LIMIT_BIG;
