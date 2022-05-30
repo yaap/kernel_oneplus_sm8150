d67ac33df312ced5c294a16b532aee10061565c0
Codecity001 <sanganiharish263@gmail.com>
2022-05-30 18:01:13 +0530

    anykernel: Change some names

--- scripts/AnyKernel3/anykernel.sh
+++ scripts/AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=NeptuneKernel by Vwool0xE9
+kernel.string=NeptuneKernel by Codecity001|H A R I S H
 do.devicecheck=1
 do.modules=0
 do.systemless=1
