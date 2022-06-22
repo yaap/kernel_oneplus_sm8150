08b05dfdb0e1a12e28280bdf91780349e1308502
Harish <sanganiharish263@gmail.com>
2022-06-22 15:05:36 +0530

    Guard unused variables CONFIG_DEBUG_FS * This fix ammends 	https://github.com/Codecity001/Neptune_kernel_sm8150_oneplus/commit/c8f5c9cc35106784f91341325fd22e2fb5320c19 	https://github.com/Codecity001/Neptune_kernel_sm8150_oneplus/commit/b9eebe9d4a10421284ddc00d42f27489bc91aeee drivers/net/wireless/cnss_prealloc/cnss_prealloc.c:33:23: error: unused variable 'debug_base' [-Werror,-Wunused-variable] static struct dentry *debug_base;                       ^ drivers/tty/serial/msm_geni_serial.c:539:7: error: unused variable 'geni_status' [-Werror,-Wunused-variable]                 u32 geni_status = geni_read_reg_nolog(uport->membase,                     ^ drivers/tty/serial/msm_geni_serial.c:541:7: error: unused variable 'geni_ios' [-Werror,-Wunused-variable]                 u32 geni_ios = geni_read_reg_nolog(uport->membase, SE_GENI_IOS);                     ^ drivers/tty/serial/msm_geni_serial.c:542:7: error: unused variable 'rx_fifo_status' [-Werror,-Wunused-variable]                 u32 rx_fifo_status = geni_read_reg_nolog(uport->membase,                     ^ drivers/tty/serial/msm_geni_serial.c:544:7: error: unused variable 'rx_dma' [-Werror,-Wunused-variable]                 u32 rx_dma =                     ^ drivers/tty/serial/msm_geni_serial.c:629:31: error: unused variable 'port' [-Werror,-Wunused-variable]         struct msm_geni_serial_port *port = GET_DEV_PORT(uport);                                      ^ drivers/tty/serial/msm_geni_serial.c:794:31: error: unused variable 'port' [-Werror,-Wunused-variable]         struct msm_geni_serial_port *port = GET_DEV_PORT(uport);                                      ^ drivers/tty/serial/msm_geni_serial.c:1412:31: error: unused variable 'port' [-Werror,-Wunused-variable]         struct msm_geni_serial_port *port = GET_DEV_PORT(uport);                                      ^ drivers/tty/serial/msm_geni_serial.c:1624:31: error: unused variable 'port' [-Werror,-Wunused-variable]         struct msm_geni_serial_port *port = GET_DEV_PORT(uport);                                      ^
    Signed-off-by: Harish <sanganiharish263@gmail.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -536,6 +536,7 @@ static void wait_for_transfers_inflight(struct uart_port *uport)
 		}
 	}
 	if (check_transfers_inflight(uport)) {
+		#if defined(CONFIG_DEBUG_FS) && defined(CONFIG_IPC_LOGGING)
 		u32 geni_status = geni_read_reg_nolog(uport->membase,
 								SE_GENI_STATUS);
 		u32 geni_ios = geni_read_reg_nolog(uport->membase, SE_GENI_IOS);
@@ -543,6 +544,7 @@ static void wait_for_transfers_inflight(struct uart_port *uport)
 							SE_GENI_RX_FIFO_STATUS);
 		u32 rx_dma =
 			geni_read_reg_nolog(uport->membase, SE_DMA_RX_LEN_IN);
+		#endif
 
 		IPC_LOG_MSG(port->ipc_log_misc,
 			"%s IOS 0x%x geni status 0x%x rx: fifo 0x%x dma 0x%x\n",
@@ -626,7 +628,9 @@ static int msm_geni_serial_ioctl(struct uart_port *uport, unsigned int cmd,
 
 static void msm_geni_serial_break_ctl(struct uart_port *uport, int ctl)
 {
+	#if defined(CONFIG_DEBUG_FS) && defined(CONFIG_IPC_LOGGING)
 	struct msm_geni_serial_port *port = GET_DEV_PORT(uport);
+	#endif
 
 	if (!uart_console(uport) && device_pending_suspend(uport)) {
 		IPC_LOG_MSG(port->ipc_log_misc,
@@ -791,7 +795,9 @@ static int msm_geni_serial_power_on(struct uart_port *uport)
 
 static void msm_geni_serial_power_off(struct uart_port *uport)
 {
+	#if defined(CONFIG_DEBUG_FS) && defined(CONFIG_IPC_LOGGING)
 	struct msm_geni_serial_port *port = GET_DEV_PORT(uport);
+	#endif
 	int usage_count = atomic_read(&uport->dev->power.usage_count);
 
 	if (!usage_count) {
@@ -1409,7 +1415,9 @@ static void stop_tx_sequencer(struct uart_port *uport)
 
 static void msm_geni_serial_stop_tx(struct uart_port *uport)
 {
+	#if defined(CONFIG_DEBUG_FS) && defined(CONFIG_IPC_LOGGING)
 	struct msm_geni_serial_port *port = GET_DEV_PORT(uport);
+	#endif
 
 	if (!uart_console(uport) && device_pending_suspend(uport)) {
 		dev_err(uport->dev, "%s.Device is suspended.\n", __func__);
@@ -1621,7 +1629,9 @@ exit_rx_seq:
 
 static void msm_geni_serial_stop_rx(struct uart_port *uport)
 {
+	#if defined(CONFIG_DEBUG_FS) && defined(CONFIG_IPC_LOGGING)
 	struct msm_geni_serial_port *port = GET_DEV_PORT(uport);
+	#endif
 
 	if (!uart_console(uport) && device_pending_suspend(uport)) {
 		IPC_LOG_MSG(port->ipc_log_misc,
