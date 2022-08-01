f1a1961397e745fab7c014dcb53dc950c426eaab
Harish <sanganiharish263@gmail.com>
2022-08-01 12:03:10 +0000

    Revert "kswapd: move to 8 threads"
    This reverts commit 1afb22bce6297051dfeab43b329637eaa240839b.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -131,7 +131,7 @@ struct scan_control {
 /*
  * Number of active kswapd threads
  */
-#define DEF_KSWAPD_THREADS_PER_NODE 8
+#define DEF_KSWAPD_THREADS_PER_NODE 4
 int kswapd_threads = DEF_KSWAPD_THREADS_PER_NODE;
 int kswapd_threads_current = DEF_KSWAPD_THREADS_PER_NODE;
 
