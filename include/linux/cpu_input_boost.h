3f9f7c6dcdfd30546ee014995b81921c791f6620
John Galt <johngaltfirstrun@gmail.com>
2023-06-07 12:16:43 -0400

    cpufreq/cpu_input_boost: checkout to https://github.com/yaap/kernel_oneplus_sm8150/commit/13d68c16d0d375f60f693fb12ab12ca0e7cc0a16

---
+++ include/linux/cpu_input_boost.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2018-2019 Sultan Alsawaf <sultan@kerneltoast.com>.
+ */
+#ifndef _CPU_INPUT_BOOST_H_
+#define _CPU_INPUT_BOOST_H_
+
+#ifdef CONFIG_CPU_INPUT_BOOST
+extern unsigned long last_input_time;
+
+void cpu_input_boost_kick(void);
+void cpu_input_boost_kick_max(unsigned int duration_ms, bool always);
+#else
+static inline void cpu_input_boost_kick(void)
+{
+}
+static inline void cpu_input_boost_kick_max(unsigned int duration_ms, bool always)
+{
+}
+#endif
+
+#endif /* _CPU_INPUT_BOOST_H_ */
