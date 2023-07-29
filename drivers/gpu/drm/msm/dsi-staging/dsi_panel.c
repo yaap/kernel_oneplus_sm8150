b6ae576743e138eb5beb881a0a810c63d971fef5
John Galt <johngaltfirstrun@gmail.com>
2023-07-29 12:52:24 -0400

    Revert "display/get*dim_alpha: drop unnecessary locks"
    This reverts commit 3e4aa1ebade37babdccc9034185e4cb9e8d97b71.

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -1026,8 +1026,10 @@ u8 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel)
 	u8 fodalpha;
 	u8 dcalpha;
 
+	mutex_lock(&panel->panel_lock);
 	fodalpha = panel->fod_dim_alpha;
 	dcalpha = panel->dc_dim_alpha;
+	mutex_unlock(&panel->panel_lock);
 
 	if (dcalpha)
 		alpha = dcalpha;
@@ -1041,7 +1043,9 @@ u8 dsi_panel_get_dc_dim_alpha(struct dsi_panel *panel)
 {
 	u8 alpha;
 
+	mutex_lock(&panel->panel_lock);
 	alpha = panel->dc_dim_alpha;
+	mutex_unlock(&panel->panel_lock);
 
 	return alpha;
 }
