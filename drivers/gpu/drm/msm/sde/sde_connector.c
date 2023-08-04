36e886d1bcc1a3f78ced27d37da85e09c4aff0b2
John Galt <johngaltfirstrun@gmail.com>
2023-08-03 20:17:44 -0400

    sched/core: improve clamp boosting readability
    And readabiliyy

--- drivers/gpu/drm/msm/sde/sde_connector.c
+++ drivers/gpu/drm/msm/sde/sde_connector.c
@@ -67,8 +67,6 @@ static const struct drm_prop_enum_list e_qsync_mode[] = {
 	{SDE_RM_QSYNC_ONE_SHOT_MODE,	"one_shot"},
 };
 
-unsigned long last_fod_time;
-
 static int sde_backlight_device_update_status(struct backlight_device *bd)
 {
 	int brightness;
@@ -675,7 +673,6 @@ static void sde_connector_pre_update_fod_hbm(struct sde_connector *c_conn)
 		cpu_input_boost_kick_max(1200, true);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1200, true);
 		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 1200, true);
-		last_fod_time = jiffies;
 		if (panel->bl_config.bl_level > 1023 || HBM_flag == true)
 			was_hbm = true;
 		else
