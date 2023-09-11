ab32f30b70273c923d0723e6dfe254f2bf6fa3a4
John Galt <johngaltfirstrun@gmail.com>
2023-09-11 10:18:10 -0400

    msm_drv: move all prime affines to hp mask

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -814,7 +814,7 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 		kthread_init_worker(&priv->disp_thread[i].worker);
 		priv->disp_thread[i].dev = ddev;
 		priv->disp_thread[i].thread =
-			kthread_run_perf_critical(cpu_prime_mask,
+			kthread_run_perf_critical(cpu_hp_mask,
 				kthread_worker_fn,
 				&priv->disp_thread[i].worker,
 				"crtc_commit:%d", priv->disp_thread[i].crtc_id);
@@ -830,7 +830,7 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 		kthread_init_worker(&priv->event_thread[i].worker);
 		priv->event_thread[i].dev = ddev;
 		priv->event_thread[i].thread =
-			kthread_run_perf_critical(cpu_prime_mask,
+			kthread_run_perf_critical(cpu_hp_mask,
 				kthread_worker_fn,
 				&priv->event_thread[i].worker,
 				"crtc_event:%d", priv->event_thread[i].crtc_id);
@@ -874,7 +874,7 @@ static int msm_drm_init(struct device *dev, struct drm_driver *drv)
 	 * other important events.
 	 */
 	kthread_init_worker(&priv->pp_event_worker);
-	priv->pp_event_thread = kthread_run_perf_critical(cpu_prime_mask,
+	priv->pp_event_thread = kthread_run_perf_critical(cpu_hp_mask,
 			kthread_worker_fn, &priv->pp_event_worker, "pp_event");
 	kthread_queue_work(&priv->pp_event_worker, &priv->thread_priority_work);
 
